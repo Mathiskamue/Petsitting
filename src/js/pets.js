fe66139d32568c451f1b6565c19edc54a5204e0d
Mathis Kamue <mathiskamue@gmail.com>
2018-10-25 11:10:07 +0200

    MathisNewPets

--- src/js/pets.js
+++ src/js/pets.js
@@ -1,24 +1,23 @@
 window.addEventListener("load", function() {
-    var btnhinzu = document.getElementById('btnhinzu');
-    var modaljs = document.getElementById('modaljs');
-    var btnzu = document.getElementById('btnzu');
+    let hmodal = document.getElementById("hmodal");
+    let hinzu = document.getElementById("hinzu");
+    let close = document.getElementsByClassName("close")[0];
 
-    btnhinzu.onclick = function() {
-        modaljs.style.display = "block";
 
-    }
-    btnzu.onclick = function() {
-        modaljs.style.display = "none";
-    }
-    window.onclick = function(event) {
-        if (event.target == modaljs) {
-            modaljs.style.display = "none";
+    hinzu.addEventListener("click", function () {
+        hmodal.style.display = "block";
+    });
+
+
+    close.addEventListener("click", function () {
+        hmodal.style.display = "none";
+    });
+
+    window.addEventListener("click", function() {
+        if (event.target == hmodal) {
+            hmodal.style.display = "none";
         }
-    }
-
-
-
-
+    });
 
 
 });
