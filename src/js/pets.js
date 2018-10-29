546514e6b42841c47a2bf0785d71917dad145174
Mathis Kamue <mathiskamue@gmail.com>
2018-10-29 12:17:52 +0100

    MathisPetsEintrag1

--- src/js/pets.js
+++ src/js/pets.js
@@ -4,16 +4,29 @@ window.addEventListener("load", function() {
     let hmodal = document.getElementById("hmodal");
     let hinzu = document.getElementById("hinzu");
 
+    //Template
+    let tentry = document.getElementById("tentry").innerHTML;
     //Feld
     let name = document.getElementById("name");
     let art = document.getElementById("art");
     let rasse = document.getElementById("rasse");
     let wohn = document.getElementById("wohn");
+    let email = document.getElementById("email");
     let password = document.getElementById("password");
     let vom = document.getElementById("vom");
     let bis = document.getElementById("bis");
     let comment = document.getElementById("comment");
     let submitBtn = document.getElementById("submitBtn");
+
+    let tester = document.getElementById("tester");
+    tester.addEventListener("click",function(){
+        let dummy = document.createElement("div");
+        dummy.innerHTML = tentry;
+        dummy.innerHTML = dummy.innerHTML.replace("$Name1$", "Überschrift");
+        document.getElementById("eintrag").appendChild(dummy);
+
+    });
+
 
     //OPEN-CLOSE-MODAL
     hinzu.addEventListener("click", function () {
@@ -40,6 +53,7 @@ window.addEventListener("load", function() {
         vom.style.border = '1px solid #BEBEBE';
         bis.style.border = '1px solid #BEBEBE';
         password.style.border = '1px solid #BEBEBE';
+        email.style.border = '1px solid #BEBEBE';
 
         let fehler = 0;
         if(name.value==""){
@@ -66,6 +80,10 @@ window.addEventListener("load", function() {
             password.style.border = '1px solid red';
             fehler += 1;
         }
+        if(email.value==""){
+            email.style.border = '1px solid red';
+            fehler += 1;
+        }
         if(fehler==0){
             let ref = firebase.database().ref("eintrag");
             ref.once("value")
@@ -80,6 +98,7 @@ window.addEventListener("load", function() {
                        art: art.value,
                        rasse: rasse.value,
                        wohnort: wohn.value,
+                       email: email.value,
                        passwort: password.value,
                        kommentar: comment.value,
                        zeitraum:{
