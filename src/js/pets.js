208c8544332c46316d987d7e5b879db1fc258e32
Mathis Kamue <mathiskamue@gmail.com>
2018-10-31 11:42:46 +0100

    MathisPets2inPets

--- src/js/pets.js
+++ src/js/pets.js
@@ -24,33 +24,134 @@ window.addEventListener("load", function() {
         ref.once("value")
             .then(function(snapshot) {
                 let zahl = snapshot.child("highest").val();
-                for(let i = 1; i<zahl;i++){
+                if(zahl%2==0){
+                    for(let i = 1; i<zahl;i++){
+                        let dummy = document.createElement("div");
+                        dummy.innerHTML = tentry;
+                        dummy.innerHTML = dummy.innerHTML.replace("$Name1$",snapshot.child(i).child("name").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Art1$",snapshot.child(i).child("art").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Rasse1$",snapshot.child(i).child("rasse").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Wohnort1$",snapshot.child(i).child("wohnort").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Email1$", snapshot.child(i).child("email").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$vom1$", snapshot.child(i).child("zeitraum").child("Von").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$bis1$", snapshot.child(i).child("zeitraum").child("Bis").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$kommentar1$", snapshot.child(i).child("kommentar").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$ID1$", snapshot.child(i).child("ID").val());
+
+                        dummy.innerHTML = dummy.innerHTML.replace("$Name2$",snapshot.child(i+1).child("name").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Art2$",snapshot.child(i+1).child("art").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Rasse2$",snapshot.child(i+1).child("rasse").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Wohnort2$",snapshot.child(i+1).child("wohnort").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Email2$", snapshot.child(i+1).child("email").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$vom2$", snapshot.child(i+1).child("zeitraum").child("Von").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$bis2$", snapshot.child(i+1).child("zeitraum").child("Bis").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$kommentar2$", snapshot.child(i+1).child("kommentar").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$ID2$", snapshot.child(i+1).child("ID").val());
+
+                        document.getElementById("eintrag").appendChild(dummy);
+
+                        document.getElementById("eintrag1").id= snapshot.child(i).child("ID").val() + "entry";
+                        document.getElementById("name1").id= snapshot.child(i).child("ID").val() + "name";
+                        document.getElementById("art1").id= snapshot.child(i).child("ID").val() + "art";
+                        document.getElementById("rasse1").id= snapshot.child(i).child("ID").val() + "rasse";
+                        document.getElementById("wohnort1").id= snapshot.child(i).child("ID").val() + "wohnort";
+                        document.getElementById("email1").id= snapshot.child(i).child("ID").val() + "email";
+                        document.getElementById("zeitraum1").id= snapshot.child(i).child("ID").val() + "zeitraum";
+                        document.getElementById("kommentar1").id= snapshot.child(i).child("ID").val() + "kommentar";
+                        document.getElementById("loesch1").id= snapshot.child(i).child("ID").val() + "loesch";
+
+                        document.getElementById("eintrag2").id= snapshot.child(i+1).child("ID").val() + "entry";
+                        document.getElementById("name2").id= snapshot.child(i+1).child("ID").val() + "name";
+                        document.getElementById("art2").id= snapshot.child(i+1).child("ID").val() + "art";
+                        document.getElementById("rasse2").id= snapshot.child(i+1).child("ID").val() + "rasse";
+                        document.getElementById("wohnort2").id= snapshot.child(i+1).child("ID").val() + "wohnort";
+                        document.getElementById("email2").id= snapshot.child(i+1).child("ID").val() + "email";
+                        document.getElementById("zeitraum2").id= snapshot.child(i+1).child("ID").val() + "zeitraum";
+                        document.getElementById("kommentar2").id= snapshot.child(i+1).child("ID").val() + "kommentar";
+                        document.getElementById("loesch2").id= snapshot.child(i+1).child("ID").val() + "loesch";
+
+
+                        i++;
+                    }
+                }
+                else{
+                    for(let i = 1; i<(zahl-1);i++){
+                        let dummy = document.createElement("div");
+                        dummy.innerHTML = tentry;
+                        dummy.innerHTML = dummy.innerHTML.replace("$Name1$",snapshot.child(i).child("name").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Art1$",snapshot.child(i).child("art").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Rasse1$",snapshot.child(i).child("rasse").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Wohnort1$",snapshot.child(i).child("wohnort").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Email1$", snapshot.child(i).child("email").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$vom1$", snapshot.child(i).child("zeitraum").child("Von").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$bis1$", snapshot.child(i).child("zeitraum").child("Bis").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$kommentar1$", snapshot.child(i).child("kommentar").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$ID1$", snapshot.child(i).child("ID").val());
+
+                        dummy.innerHTML = dummy.innerHTML.replace("$Name2$",snapshot.child(i+1).child("name").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Art2$",snapshot.child(i+1).child("art").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Rasse2$",snapshot.child(i+1).child("rasse").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Wohnort2$",snapshot.child(i+1).child("wohnort").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$Email2$", snapshot.child(i+1).child("email").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$vom2$", snapshot.child(i+1).child("zeitraum").child("Von").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$bis2$", snapshot.child(i+1).child("zeitraum").child("Bis").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$kommentar2$", snapshot.child(i+1).child("kommentar").val());
+                        dummy.innerHTML = dummy.innerHTML.replace("$ID2$", snapshot.child(i+1).child("ID").val());
+
+                        document.getElementById("eintrag").appendChild(dummy);
+
+                        document.getElementById("eintrag1").id= snapshot.child(i).child("ID").val() + "entry";
+                        document.getElementById("name1").id= snapshot.child(i).child("ID").val() + "name";
+                        document.getElementById("art1").id= snapshot.child(i).child("ID").val() + "art";
+                        document.getElementById("rasse1").id= snapshot.child(i).child("ID").val() + "rasse";
+                        document.getElementById("wohnort1").id= snapshot.child(i).child("ID").val() + "wohnort";
+                        document.getElementById("email1").id= snapshot.child(i).child("ID").val() + "email";
+                        document.getElementById("zeitraum1").id= snapshot.child(i).child("ID").val() + "zeitraum";
+                        document.getElementById("kommentar1").id= snapshot.child(i).child("ID").val() + "kommentar";
+                        document.getElementById("loesch1").id= snapshot.child(i).child("ID").val() + "loesch";
+
+                        document.getElementById("eintrag2").id= snapshot.child(i+1).child("ID").val() + "entry";
+                        document.getElementById("name2").id= snapshot.child(i+1).child("ID").val() + "name";
+                        document.getElementById("art2").id= snapshot.child(i+1).child("ID").val() + "art";
+                        document.getElementById("rasse2").id= snapshot.child(i+1).child("ID").val() + "rasse";
+                        document.getElementById("wohnort2").id= snapshot.child(i+1).child("ID").val() + "wohnort";
+                        document.getElementById("email2").id= snapshot.child(i+1).child("ID").val() + "email";
+                        document.getElementById("zeitraum2").id= snapshot.child(i+1).child("ID").val() + "zeitraum";
+                        document.getElementById("kommentar2").id= snapshot.child(i+1).child("ID").val() + "kommentar";
+                        document.getElementById("loesch2").id= snapshot.child(i+1).child("ID").val() + "loesch";
+
+
+                        i++;
+                    }
                     let dummy = document.createElement("div");
                     dummy.innerHTML = tentry;
-                    dummy.innerHTML = dummy.innerHTML.replace("$Name1$",snapshot.child(i).child("name").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Art1$",snapshot.child(i).child("art").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Rasse1$",snapshot.child(i).child("rasse").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Wohnort1$",snapshot.child(i).child("wohnort").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Email1$", snapshot.child(i).child("email").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$vom1$", snapshot.child(i).child("zeitraum").child("Von").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$bis1$", snapshot.child(i).child("zeitraum").child("Bis").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$kommentar1$", snapshot.child(i).child("kommentar").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$ID1$", snapshot.child(i).child("ID").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$Name1$",snapshot.child(zahl).child("name").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$Art1$",snapshot.child(zahl).child("art").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$Rasse1$",snapshot.child(zahl).child("rasse").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$Wohnort1$",snapshot.child(zahl).child("wohnort").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$Email1$", snapshot.child(zahl).child("email").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$vom1$", snapshot.child(zahl).child("zeitraum").child("Von").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$bis1$", snapshot.child(zahl).child("zeitraum").child("Bis").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$kommentar1$", snapshot.child(zahl).child("kommentar").val());
+                    dummy.innerHTML = dummy.innerHTML.replace("$ID1$", snapshot.child(zahl).child("ID").val());
 
-                    dummy.innerHTML = dummy.innerHTML.replace("$Name2$",snapshot.child(i+1).child("name").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Art2$",snapshot.child(i+1).child("art").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Rasse2$",snapshot.child(i+1).child("rasse").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Wohnort2$",snapshot.child(i+1).child("wohnort").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$Email2$", snapshot.child(i+1).child("email").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$vom2$", snapshot.child(i+1).child("zeitraum").child("Von").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$bis2$", snapshot.child(i+1).child("zeitraum").child("Bis").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$kommentar2$", snapshot.child(i+1).child("kommentar").val());
-                    dummy.innerHTML = dummy.innerHTML.replace("$ID2$", snapshot.child(i+1).child("ID").val());
 
                     document.getElementById("eintrag").appendChild(dummy);
 
-                    i++;
+                    document.getElementById("eintrag1").id= snapshot.child(zahl).child("ID").val() + "entry";
+                    document.getElementById("name1").id= snapshot.child(zahl).child("ID").val() + "name";
+                    document.getElementById("art1").id= snapshot.child(zahl).child("ID").val() + "art";
+                    document.getElementById("rasse1").id= snapshot.child(zahl).child("ID").val() + "rasse";
+                    document.getElementById("wohnort1").id= snapshot.child(zahl).child("ID").val() + "wohnort";
+                    document.getElementById("email1").id= snapshot.child(zahl).child("ID").val() + "email";
+                    document.getElementById("zeitraum1").id= snapshot.child(zahl).child("ID").val() + "zeitraum";
+                    document.getElementById("kommentar1").id= snapshot.child(zahl).child("ID").val() + "kommentar";
+                    document.getElementById("loesch1").id= snapshot.child(zahl).child("ID").val() + "loesch";
+
+                    document.getElementById("eintrag2").style.display="none";
                 }
+
+
 
 
            });
@@ -63,6 +164,7 @@ window.addEventListener("load", function() {
         //document.getElementById("eintrag").appendChild(dummy);
 
     });
+
 
 
     //OPEN-CLOSE-MODAL
@@ -81,9 +183,12 @@ window.addEventListener("load", function() {
         }
     });
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     //in Datenbank übergeben
-    submitBtn.addEventListener("click", function() {
+    submitBtn.addEventListener("click", async function() {
         name.style.border = '1px solid #BEBEBE';
         art.style.border = '1px solid #BEBEBE';
         wohn.style.border = '1px solid #BEBEBE';
@@ -146,7 +251,8 @@ window.addEventListener("load", function() {
                     ref.child("highest").set(zahl);
                });
                hmodal.style.display = "none";
-
+               await sleep(1000);
+               document.location.reload(true);
         }
         else {
             alert('Bitte füllen Sie alle Pflichtfelder aus!');
